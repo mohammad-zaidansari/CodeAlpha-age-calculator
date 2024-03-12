fbf822af26bf860272e295af9f1857f59188f4fc
Mohammad Zaid Ansari <mzaidansari712@gmail.com>
2024-03-12 12:09:37 +0530

    some changes

--- app.js
+++ app.js
@@ -1,47 +1,20 @@
-let userInput = document.getElementById("date");
-userInput.max = new Date().toISOString().split("T")[0]; //adding this line we can't select future date these are disable.
-let result = document.getElementById("result");
+function calculateAge() {
+    let birthdateString = document.getElementById("DOBdate").value;
+    let birthdate = new Date(birthdateString);
+    let currentDate = new Date();
 
-function ageCal(){
-    let dateOB = new Date(userInput.value);
-
-    let d1 = dateOB.getDate();
-    let m1 = dateOB.getMonth() + 1;
-    let y1 = dateOB.getFullYear();
-
-    let todayDate = new Date();
-
-    let d2 = todayDate.getDate();
-    let m2 = todayDate.getMonth() + 1;
-    let y2 = todayDate.getFullYear();
-
-    let d3, m3, y3;
-
-    y3 = y2 - y1;
-       
-    if(m2 >= m1){
-        m3 = m2 = m1;
+    if(birthdateString == ""){
+        alert("Please enter your date of birth");
     }else{
-        y3--;
-        m3 = 12 + m2 - m1;
+        let ageInMilliseconds = currentDate - birthdate;
+
+        let ageInYears = Math.floor(ageInMilliseconds / (1000 * 60 * 60 * 24 * 365.25));
+        let ageInMonths = Math.floor((ageInMilliseconds % (1000 * 60 * 60 * 24 * 365.25)) / (1000 * 60 * 60 * 24 * (365.25 / 12)));
+        let ageInDays = Math.floor((ageInMilliseconds % (1000 * 60 * 60 * 24 * (365.25 / 12))) / (1000 * 60 * 60 * 24));
+    
+        let resultElement = document.getElementById("result");
+        resultElement.innerHTML = `Your age is ${ageInYears}  years, ${ageInMonths} months, and ${ageInDays} days`;
     }
 
-    if(d2 >= d1){
-        d3 = d2 - m1;
-    }else{
-        m3--;
-        d3 = getDaysInMonth(y1, m1) + d2 - d1;
-    }
-
-    if(m3 < 0){
-        m3 = 11;
-        y3--; 
-    }
-    // console.log(y3, m3, d3);
-
-    result.innerHTML = `You are ${y3} years, ${m3} months and ${d3} days old`
-}
-
-function getDaysInMonth(year, month){
-    return new Date(year, month, 0).getDate();
+    
 }
